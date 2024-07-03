e7e4254638f23a9251764e2b606524ebc5d8fa66
Jesper Lund <jlu@onlinecity.dk>
2024-07-03 11:14:01 +0200

    Fix wording, cleanup and set right python version

--- devbox-plugins/base-config/config/ansible-requirements-helper.sh
+++ devbox-plugins/base-config/config/ansible-requirements-helper.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# This script is used to merge the ansible-requirements.txt.dist and configure/requirements.txt.local files
+# This script is managed from devbox. Do not touch.
+# Used to merge the ansible-requirements.txt.dist and configure/requirements.txt.local files
 
-set -eux
+set -e
 
 # If we have a configure/requirements.txt.local file, we merge it with the ansible-requirements.txt.dist file.
 if [ -e configure/requirements.txt.local ]; then
     rm -f requirements-merged.txt
     merge_requirements {{ .Virtenv }}/ansible-requirements.txt.dist configure/requirements.txt.local
     mv requirements-merged.txt configure/requirements.txt
-    # pyreq --output configure/requirements.txt --method upgrade {{ .Virtenv }}/ansible-requirements.txt.dist configure/requirements.txt.local
 else
-    # Or else we simply just use the ansible-requirements.txt.dist file provided fomr devbox.
+    # Or else we simply just use the ansible-requirements.txt.dist file provided from devbox.
     cp {{ .Virtenv }}/ansible-requirements.txt.dist configure/requirements.txt
 fi
 
