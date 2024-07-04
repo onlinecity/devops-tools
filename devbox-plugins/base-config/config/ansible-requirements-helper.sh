19e9bd0d8355ccada5d13b860ba0ea62086b3067
Bue Petersen <1579826+buep@users.noreply.github.com>
2024-07-04 15:20:12 +0200

    fix: Add about Python req. and fix ansible-bootstrap
    Add some extra information about the management of Python packages and
    how they are added to the projects requirements.txt Python package file.
    
    Improve the 'ansible-requirements' devbox command to add traceability
    information, and the merge process strips any comments.
    
    Also fix the ansible-bootstrap command to ensure the requirements files
    are in place, if not already in place.
    
    Minor wording and spelling fixed as well.

--- devbox-plugins/base-config/config/ansible-requirements-helper.sh
+++ devbox-plugins/base-config/config/ansible-requirements-helper.sh
@@ -7,11 +7,23 @@ set -e
 
 # If we have a configure/requirements.txt.local file, we merge it with the ansible-requirements.txt.dist file.
 if [ -e configure/requirements.txt.local ]; then
+    # remove old file first, if it exists because merge_requirements will not overwrite and create new ones
     rm -f requirements-merged.txt
+    # produced requirements-merged.txt in the root of the repository
     merge_requirements {{ .Virtenv }}/ansible-requirements.txt.dist configure/requirements.txt.local
-    mv requirements-merged.txt configure/requirements.txt
+
+    # Add little helper and traceability msg and output to final file and then add merged result
+    cat << EOF > configure/requirements.txt
+# This file is the result of a merge of requirements files, always taking the union of Python packages, and the last version of each package.
+# The merge is done by our devops tools, and a devbox run command merging two files:
+# -  {{ .Virtenv }}/ansible-requirements.txt.dist
+# - configure/requirements.txt.local
+# NOTICE comments are stripped out, so check the two files for comments if in doubt of why packages are included.
+EOF
+    cat requirements-merged.txt >> configure/requirements.txt
+    rm requirements-merged.txt
 else
-    # Or else we simply just use the ansible-requirements.txt.dist file provided from devbox.
+    # Or else we simply just use the ansible-requirements.txt.dist file provided from devbox (this also contains comments and traceability information)
     cp {{ .Virtenv }}/ansible-requirements.txt.dist configure/requirements.txt
 fi
 
